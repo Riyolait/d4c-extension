dab336d8c2710f66adfe579a7cdd81a5e7051215
Matthieu <matthieu.serek@etu.univ-lyon1.fr>
2023-10-26 17:49:56 +0200

    Extension v1.31

---
+++ i18n/d4c_api_en.ts
@@ -0,0 +1,306 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="fr_FR">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="d4c_api_dialog_add_dataset.py" line="71"/>
+        <source>Créer un Jeu de Donnée</source>
+        <translation>Create a new Dataset</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_add_dataset.py" line="72"/>
+        <source>Créer un nouveau Jeu de Donnée :</source>
+        <translation>Create a new Dataset :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_add_dataset.py" line="73"/>
+        <source>Privée</source>
+        <translation>Private</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_add_dataset.py" line="74"/>
+        <source>Créer</source>
+        <translation>Add</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_add_dataset.py" line="75"/>
+        <source>Organisation :</source>
+        <translation>Organization :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_add_dataset.py" line="76"/>
+        <source>Nom du Jeu de donnée :</source>
+        <translation>Name of the dataset :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_add_dataset.py" line="77"/>
+        <source>Site D4C actuel :</source>
+        <translation>Actual D4C Website :</translation>
+    </message>
+</context>
+<context>
+    <name>EditSites</name>
+    <message>
+        <location filename="d4c_api_dialog_editsites.py" line="124"/>
+        <source>Site</source>
+        <translation>Website</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_editsites.py" line="125"/>
+        <source>Valider</source>
+        <translation>Ok</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_editsites.py" line="126"/>
+        <source>Utilisateur:</source>
+        <translation>User :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_editsites.py" line="127"/>
+        <source>ex : https://website.data4citizen.com</source>
+        <translation>ex : https://website.data4citizen.com</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_editsites.py" line="128"/>
+        <source>URL du Site :</source>
+        <translation>Website URL :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_editsites.py" line="129"/>
+        <source>Mot de Passe :</source>
+        <translation>Password :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_editsites.py" line="130"/>
+        <source>Nom :</source>
+        <translation>Name :</translation>
+    </message>
+</context>
+<context>
+    <name>LoginsWindow</name>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="138"/>
+        <source>Connexion</source>
+        <translation>Connect</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="139"/>
+        <source>Mot de Passe :</source>
+        <translation>Password :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="140"/>
+        <source>URL du Site :</source>
+        <translation>Website URL :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="145"/>
+        <source>Se connecter</source>
+        <translation>Connect</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="142"/>
+        <source>ex : https://website.data4citizen.com</source>
+        <translation>ex : https://website.data4citizen.com</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="143"/>
+        <source>Utilisateur:</source>
+        <translation>User :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="144"/>
+        <source>Sites enregistrées :</source>
+        <translation>Saved Sites :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_logins.py" line="146"/>
+        <source>Gérer les sites</source>
+        <translation>My Saved Sites</translation>
+    </message>
+</context>
+<context>
+    <name>ShowSites</name>
+    <message>
+        <location filename="d4c_api_dialog_showsites.py" line="66"/>
+        <source>Mes Sites</source>
+        <translation>My Sites</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_showsites.py" line="67"/>
+        <source>Ajouter</source>
+        <translation>Add</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_showsites.py" line="68"/>
+        <source>Modifier</source>
+        <translation>Change</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_showsites.py" line="69"/>
+        <source>Supprimer</source>
+        <translation>Delete</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_showsites.py" line="70"/>
+        <source>Fermer</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_showsites.py" line="71"/>
+        <source>Dupliquer</source>
+        <translation>Duplicate</translation>
+    </message>
+</context>
+<context>
+    <name>d4cAPI</name>
+    <message>
+        <location filename="d4c_api.py" line="248"/>
+        <source>&amp;D4C Plugin</source>
+        <translation>&amp;D4C Plugin</translation>
+    </message>
+    <message>
+        <location filename="d4c_api.py" line="208"/>
+        <source>Test</source>
+        <translation>Test</translation>
+    </message>
+    <message>
+        <location filename="d4c_api.py" line="233"/>
+        <source>Data4Citizen extension</source>
+        <translation>Data4Citizen Plugin</translation>
+    </message>
+    <message>
+        <location filename="d4c_api.py" line="1605"/>
+        <source>Veuillez patienter pendant le chargement des données</source>
+        <translation>Please wait during dataset list loading</translation>
+    </message>
+</context>
+<context>
+    <name>d4cAPIDialogExport</name>
+    <message>
+        <location filename="d4c_api_dialog_export.py" line="44"/>
+        <source>Choisir une ressource</source>
+        <translation>Select a resource</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_export.py" line="45"/>
+        <source>Selectionner une ressource</source>
+        <translation>Select a resource</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_export.py" line="46"/>
+        <source>Confirmer</source>
+        <translation>Confirm</translation>
+    </message>
+</context>
+<context>
+    <name>pluginInfos</name>
+    <message>
+        <location filename="d4c_api_dialog_plugininfos.py" line="30"/>
+        <source>Informations du Plugin</source>
+        <translation>Plugin Infos</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_plugininfos.py" line="31"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Détail du plugin : &lt;/p&gt;&lt;p&gt;Version : v1.3&lt;/p&gt;&lt;p&gt;Auteur : Matthieu Serek @ BPM-conseil&lt;br/&gt;&lt;br/&gt;Contact : matthieu.serek@bpm-conseil.com&lt;/p&gt;&lt;p&gt;Git : https://github.com/Riyolait/d4c-qgis-extension&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Plugin Details : &lt;/p&gt;&lt;p&gt;Version : v1.3&lt;/p&gt;&lt;p&gt;Author: Matthieu Serek @ BPM-conseil&lt;br/&gt;&lt;br/&gt;Contact : matthieu.serek@bpm-conseil.com&lt;/p&gt;&lt;p&gt;Git : https://github.com/Riyolait/d4c-qgis-extension&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>searchDataset</name>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="111"/>
+        <source>Trouver un jeu de données</source>
+        <translation>Find a Dataset</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="112"/>
+        <source>Chercher un jeu de données</source>
+        <translation>Search a Dataset</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="113"/>
+        <source>Sélectionner</source>
+        <translation>Select</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="114"/>
+        <source>Réinitialiser les filtres</source>
+        <translation>Reset filters</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="115"/>
+        <source>Afficher uniquement GEO</source>
+        <translation>GEO Files Only</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="116"/>
+        <source>Filtres :</source>
+        <translation>Filters :</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="117"/>
+        <source>Fermer</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="118"/>
+        <source>Thèmes : </source>
+        <translation>Themes : </translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="119"/>
+        <source>Mot-clé : </source>
+        <translation>Tags : </translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_searchdataset.py" line="120"/>
+        <source>Choisir une organisation</source>
+        <translation>Choose organization</translation>
+    </message>
+</context>
+<context>
+    <name>showInfos</name>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="105"/>
+        <source>Informations</source>
+        <translation>Infos</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="102"/>
+        <source>Fermer</source>
+        <translation>Close</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="103"/>
+        <source>Auteur</source>
+        <translation>Author</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="104"/>
+        <source>Privé</source>
+        <translation>Private</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="106"/>
+        <source>Titre</source>
+        <translation>Title</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="107"/>
+        <source>Licence</source>
+        <translation>License</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="108"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+    <message>
+        <location filename="d4c_api_dialog_infos.py" line="109"/>
+        <source>Organisation</source>
+        <translation>Organization</translation>
+    </message>
+</context>
+</TS>
